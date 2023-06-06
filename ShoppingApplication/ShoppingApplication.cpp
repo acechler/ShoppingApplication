89b9b8226a1d2a7e29a24cb37e27fc3fbd63dff4
acech <acechler@gmail.com>
2023-06-06 10:20:54 -0400

    Implemented a user struct that holds cash and a list of products.

--- ShoppingApplication/ShoppingApplication.cpp
+++ ShoppingApplication/ShoppingApplication.cpp
@@ -2,106 +2,147 @@
 #include <string>
 #include <unordered_map>
 #include <regex>
+#include <vector>
+
 
 struct Product {
-    std::string name;
-    double price = 0;
-    int quantity = 0;
+	std::string name;
+	double price = 0;
+	int quantity = 0;
 };
 
+struct Customer {
+	double cash;
+	std::vector<Product> cart;
+};
 struct Menu {
-    std::string title;
-    std::string instructions;
-    std::unordered_map<int, Product> items;
-    bool active;
+	std::string title;
+	std::string instructions;
+	std::unordered_map<int, Product> items;
+	bool active;
 };
 
 class ShoppingApplication {
 private:
-    void populateMenu(Menu& menu) {
-        // Add products to the menu
-        menu.title = { "Available Products" };
-        menu.items = {
-            {1, {"Product 1", 10.99, 5}},
-            {2, {"Product 2", 5.99, 10}},
-            {3, {"Product 3", 8.99, 3}},
-            // Add more products as needed
-        };
-        menu.active = { true };
-    }
+	void populateMenu(Menu& menu) {
+		// Add products to the menu
+		menu.title = { "Available Products" };
+		menu.items = {
+			{1, {"Product 1", 10.99, 5}},
+			{2, {"Product 2", 5.99, 10}},
+			{3, {"Product 3", 8.99, 3}},
+			// Add more products as needed
+		};
+		menu.active = { true };
+	}
 
-    void displayMenu(const Menu& menu) const {
-        std::cout << menu.title << std::endl << std::endl;
-        std::cout << menu.instructions << std::endl;
+	void displayMenu(const Menu& menu) const {
+		std::cout << menu.title << std::endl << std::endl;
+		std::cout << menu.instructions << std::endl;
 
-        for (const auto& item : menu.items) {
-            const Product& product = item.second;
-            std::cout << item.first << ". " << product.name << " - $" << product.price << " (" << product.quantity << " left)" << std::endl;
-        }
-    }
+		for (const auto& item : menu.items) {
+			const Product& product = item.second;
+			std::cout << item.first << ". " << product.name << " - $" << product.price << " (" << product.quantity << " left)" << std::endl;
+		}
+	}
 
-    // Handles user input extraction
-    int getUserInput(const std::string& prompt, const std::regex& validationRegex) const {
-        std::string input;
-        std::cout << prompt;
-        std::getline(std::cin, input);
-        while (!std::regex_match(input, validationRegex) || input.empty() || input.find_first_not_of(' ') == std::string::npos) {
-            std::cout << "Invalid input. Try again: ";
-            std::getline(std::cin, input);
-        }
-        return std::stoi(input);
-    }
+	void displayUserCart(const Customer& customer) const {
+		std::cout << "Current Items" << std::endl;
+		for (const auto& item : customer.cart) {
+			std::cout << item.name << " " << item.quantity << std::endl;
+		}
+	}
+
+	// Handles user input extraction
+	int getUserInput(const std::string& prompt, const std::regex& validationRegex) const {
+		std::string input;
+		std::cout << prompt;
+		std::getline(std::cin, input);
+		/// While Condition
+		// Check if the input is a number
+		// Check if input is empty
+		// Check if input is only spaces
+		while (!std::regex_match(input, validationRegex) || input.empty() || input.find_first_not_of(' ') == std::string::npos) {
+			std::cout << "Invalid input. Try again: ";
+			std::getline(std::cin, input);
+		}
+		return std::stoi(input);
+	}
+	int getItemAmount(const std::regex& validationRegex) const {
+		std::string input;
+		std::cout << "How many: ";
+		std::getline(std::cin, input);
+
+		/// While Condition
+		// Check if the input is a number
+		// Check if input is empty
+		// Check if input is only spaces
+		while (!std::regex_match(input, validationRegex) || input.empty() || input.find_first_not_of(' ') == std::string::npos) {
+			std::cout << "Invalid input. Try again: ";
+			std::getline(std::cin, input);
+		}
+		return std::stoi(input);
+	}
 
 public:
-    void run() {
-        bool programState = true;
+	void run() {
+		bool programState = true;
 
-        const std::regex inputValidationRegex(R"(\d+)");
-        Menu mainMenu{};
-        populateMenu(mainMenu);
+		const std::regex inputValidationRegex(R"(\d+)");
+		Menu mainMenu{};
+		Customer customer{ 50.00, {} };
+		populateMenu(mainMenu);
 
-        while (programState) {
-            while (mainMenu.active) {
-                displayMenu(mainMenu);
-                int selectedOption = getUserInput("Enter your choice: ", inputValidationRegex);
 
-                while (mainMenu.items.find(selectedOption) == mainMenu.items.end()) {
-                    std::cout << "Invalid option. Try again: ";
-                    selectedOption = getUserInput("Enter your choice: ", inputValidationRegex);
-                }
+		while (programState) {
+			while (mainMenu.active) {
 
-                const Product& selectedProduct = mainMenu.items[selectedOption];
-                
-                // !! Trying to get the quantity of requested item !!
-                selectedOption = getUserInput("How many: ", inputValidationRegex);
+				if (!customer.cart.empty()) {
+					displayUserCart(customer);
+				}
 
-                mainMenu.items[selectedOption].quantity -= 1;
-                // Perform actions based on the selected product (e.g., add to cart, update quantity, etc.)
-                
-                mainMenu.active = false;
-            }
+				displayMenu(mainMenu);
+				int selectedOption = getUserInput("Enter your choice: ", inputValidationRegex);
 
-            std::cout << "Do you want to buy another item?" << std::endl;
-            int selectedOption = getUserInput("Enter your choice: ", inputValidationRegex);
-            switch (selectedOption)
-            {
-            case 1:
-                mainMenu.active = true;
-                break;
-            case 2:
-                programState = false;
-            default:
-                break;
-            }
+				while (mainMenu.items.find(selectedOption) == mainMenu.items.end()) {
+					std::cout << "Invalid option. Try again: ";
+					selectedOption = getUserInput("Enter your choice: ", inputValidationRegex);
+				}
 
-           
-        }
-    }
+				const Product& selectedProduct = mainMenu.items[selectedOption];
+
+				// !! Trying to get the quantity of requested item !!
+				int requestedAmount = getItemAmount(inputValidationRegex);
+				
+				mainMenu.items[selectedOption].quantity -= requestedAmount;
+				// Perform actions based on the selected product (e.g., add to cart, update quantity, etc.)
+				
+				customer.cart.push_back(selectedProduct);
+				
+				mainMenu.active = false;
+			}
+
+			std::cout << "Do you want to buy another item?" << std::endl;
+			int selectedOption = getUserInput("Enter your choice: ", inputValidationRegex);
+			switch (selectedOption)
+			{
+			case 1:
+				mainMenu.active = true;
+				break;
+			case 2:
+				programState = false;
+			default:
+				break;
+			}
+
+
+		}
+	}
 };
 
 int main() {
-    
-    ShoppingApplication app;
-    app.run();
-    return 0;
+
+	ShoppingApplication app;
+	app.run();
+	return 0;
 }
